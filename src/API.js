c6abe78ad45e42075385f2c4b10f8a2408fb70a8
sumit-so <sumitso@videosdk.live>
2024-12-03 14:46:27 +0530

    end: participant recording...

--- src/API.js
+++ src/API.js
@@ -2,6 +2,8 @@ import { APP_URL } from './constants/constants';
 
 // unsafe Token
 export const authToken = import.meta.env.VITE_VIDEOSDK_TOKEN;
+
+// create a meetingId
 export const createMeeting = async () => {
   const res = await fetch(`${APP_URL}/v2/rooms`, {
     method: 'POST',
@@ -13,4 +15,48 @@ export const createMeeting = async () => {
   });
   const { roomId } = await res.json();
   return roomId;
+};
+
+// start individual recording
+export const startParticipantRecording = async (roomId, participantId) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      Authorization: authToken,
+      'Content-Type': 'application/json',
+    },
+
+    body: JSON.stringify({
+      roomId,
+      participantId,
+      fileFormat: 'webm',
+      bucketDirPath: null,
+      webhookUrl: null,
+    }),
+  };
+
+  const url = `${APP_URL}/v2/recordings/participant/start`;
+  const response = await fetch(url, options);
+  const data = await response.json();
+  console.log(data);
+};
+
+// stop individual recording
+export const stopParticipantRecording = async (roomId, participantId) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      Authorization: authToken,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      roomId,
+      participantId,
+    }),
+  };
+
+  const url = `${APP_URL}/v2/recordings/participant/stop`;
+  const response = await fetch(url, options);
+  const data = await response.json();
+  console.log(data);
 };
